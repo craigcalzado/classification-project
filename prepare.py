0d2e8539158cc13e03796d8d57ffedb13c80bed0
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-03-10 16:50:06 -0600

    Modified functions

--- prepare.py
+++ prepare.py
@@ -58,12 +58,16 @@ def split_titanic_data(df):
     return train, validate, test
 
 def prep_telco(df):
-    df = acquire.get_telco_data()
-    df = df.drop(columns=['Unnamed: 0', 'internet_service_type_id', 'payment_type_id', 'contract_type_id', 'multiple_lines'])
+    df = df.drop(columns=['Unnamed: 0', 'internet_service_type_id', 'payment_type_id', 'contract_type_id'])
     dummy_df = pd.get_dummies(df[['gender', 'payment_type', 'contract_type', 'internet_service_type']], drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
+    df['churn'] = df['churn'].map({'Yes': 1, 'No': 0})
     return df.drop(columns=['gender', 'payment_type', 'contract_type', 'internet_service_type'])
 
+def split_telco_churn(df):
+    train, test = train_test_split(df, test_size=.2, random_state=789, stratify=df.churn)
+    train, validate = train_test_split(train, test_size=.3, random_state=789, stratify=train.churn)
+    return train, validate, test
 
 # function to split dataframes
 def split_dataframe(df):
